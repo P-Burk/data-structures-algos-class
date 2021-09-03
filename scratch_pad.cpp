9724613b2bed6acca39a8109f9f4918874834806
P-Burk <68164093+P-Burk@users.noreply.github.com>
2021-09-03 14:21:27 -0400

    initial commit

---
+++ scratch_pad.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Keep calm";
+    cout << "and";         // Note: Does NOT start on new output line
+    cout << "carry on";
+
+    return 0;
+}
